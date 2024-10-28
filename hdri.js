b7a52fca14ab119905a1c9fd02042f4ca4e27191
logangr <pprohens@politecnicllevant.cat>
2024-10-28 20:47:53 +0100

    Updates

--- hdri.js
+++
@@ -1,6 +0,0 @@
-const hdriLoader = new RGBELoader();
-hdriLoader.load("hdri/curves.hdr", (texture) => {
-  texture.mapping = THREE.EquirectangularReflectionMapping;
-  scene.background = texture;
-  scene.environment = texture;
-});
